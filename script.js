7a641748f5c2ca6e94002285c3fd80fcfeec2a9f
MrHarshDugar <dugarhd.edu@gmail.com>
2025-09-17 12:47:50 +0530

    Final Adjustments

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 let youScore = 0;
 let compScore = 0;
-const msg = document.querySelector(".msgContainer");
 
+const msg = document.querySelector(".msgContainer");
 const options = document.querySelectorAll(".option");
+
+const youScorePara = document.querySelector("#youScore");
+const compScorePara = document.querySelector("#compScore");
+
+const newGameBtn = document.querySelector("#newGame");
 
 const genCompChoice = () => {
     const vikalp = ["rock", "paper", "scissor"];
@@ -11,53 +16,45 @@ const genCompChoice = () => {
 }
 
 const gameDraw = () => {
-    console.log("The Game is Draw");
     msg.innerText = "The Game is Draw! Play again.";
     msg.style.backgroundColor = "black";
     msg.style.color = "white";
+    msg.style.border = "5px outset black"
 }
 
-const compWon = () => {
-    console.log("You Lost");
-}
-
-const userWon = () => {
-    console.log("You Won");
-}
-
-const showWinner = () => {
+const showWinner = (userWon, userChose, compChose) => {
     if (userWon) {
         youScore++;
+        youScorePara.innerText = youScore;
         msg.innerText = `You Win! Your ${userChose} beats ${compChose}`;
-        msg.style.backgroundColor = "green";
+        msg.style.backgroundColor = "#72b01d";
         msg.style.color = "black";
+        msg.style.border = "5px outset #72b01d"
     } else {
         compScore++;
+        compScorePara.innerText = compScore;
         msg.innerText = `You Lost! ${compChose} beats your ${userChose}`;
-        msg.style.backgroundColor = "red";
+        msg.style.backgroundColor = "#f25757";
         msg.style.color = "black"; 
+        msg.style.border = "5px outset #f25757"
     }
 }
 
 const gamePlay = (userChose) => {
-    console.log("You chose", userChose);
     const compChose = genCompChoice();
-    console.log("Comp chose", compChose);
 
     if (userChose === compChose) {
-        return gameDraw();
-    } else if (userChose === "rock" && compChose === "paper") {
-        return compWon();
-    } else if (userChose === "rock" && compChose === "scissor") {
-        return userWon();
-    } else if (userChose === "paper" && compChose === "rock") {
-        return userWon();
-    } else if (userChose === "paper" && compChose === "scissor") {
-        return compWon();
-    } else if (userChose === "scissor" && compChose === "rock") {
-        return compWon();
-    } else if (userChose === "scissor" && compChose === "paper") {
-        return userWon();
+        gameDraw();
+    } else {
+        let userWon = true;
+        if (userChose === "rock") {
+            userWon = compChose === "paper" ? false : true;
+        } else if (userChose === "paper") {
+            userWon = compChose === "scissor" ? false : true;
+        } else {
+            userWon = compChose === "rock" ? false : true;
+        }
+        showWinner(userWon, userChose, compChose);
     }
 }
 
@@ -67,3 +64,14 @@ options.forEach((option) => {
         gamePlay(userChose);
     })
 })
+
+newGameBtn.addEventListener("click", () => {
+    youScore = 0;
+    compScore = 0;
+    youScorePara.innerText = youScore; // Reset the score display
+    compScorePara.innerText = compScore; // Reset the score display
+    msg.textContent = "Choose your Option"; // reset message
+    msg.style.backgroundColor = "pink";
+    msg.style.color = "black";
+    msg.style.border = "5px outset pink"
+});
